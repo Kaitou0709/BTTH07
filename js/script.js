67d00273175a634ba654b65f08317591f263338f
Kaitou0709 <2051063681@e.tlu.edu.vn>
2022-03-28 10:22:34 +0700

    hoàn thành

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 jQuery.validator.addMethod("lettersonly", function(value, element) 
 {
 return this.optional(element) || /^[a-z," "]+$/i.test(value);
-},); 
+},);
+jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+  });
 $(document).ready(function(){
     $("#Registrantion-Form").validate
 ({
@@ -22,6 +26,28 @@ $(document).ready(function(){
             required:true,
             lettersonly: true
         },
+        formselect:
+        {
+            required:true,
+        },
+        exampleInputZip1:
+        {
+            required:true,
+            number:true
+        },
+        exampleInputEmail1:
+        {
+            required:true,
+            email:true
+        },
+        Sex:
+        {
+            required:true
+        },
+        Language:
+        {
+            required:true
+        }
     },
     messages:
     {
@@ -40,6 +66,43 @@ $(document).ready(function(){
             required: "Vui lòng nhập tên",
             lettersonly: "Vui lòng nhập đúng tên"
         },
-    }
+        formselect:
+        {
+            required:"Vui lòng chọn phương án"
+        },
+        exampleInputZip1:
+        {
+            required:"Vui lòng nhập ZIP Code",
+            number: "Vui lòng nhập số"
+        },
+        exampleInputEmail1:
+        {
+            required:"Vui lòng nhập email",
+            email: "Email không đúng vui lòng nhập lại"
+        },
+        Sex:
+        {
+            required:"Vui lòng nhập giới tính"
+        },
+        Language:
+        {
+            required:"Vui lòng nhập ngôn ngữ"
+        }
+    },
+    errorPlacement: function(error, element) 
+        {
+            if ( element.is(":radio") ) 
+            {
+                error.appendTo( element.parents('.container') );
+            }
+            else if ( element.is(":checkbox") ) 
+            {
+                error.appendTo( element.parents('.container2') );
+            }
+            else 
+            { 
+                error.insertAfter( element );
+            }
+         }
 });
 });
